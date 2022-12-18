2d937b0c7d661f24a8fb35fb5b91cc814d721734
Sean Sundberg <seansund@us.ibm.com>
2022-12-18 16:58:41 -0600

    Adds error handling for circular iterator
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/main/java/com/dex/midi/util/CircularIterator.java
+++ src/main/java/com/dex/midi/util/CircularIterator.java
@@ -10,27 +10,33 @@ public class CircularIterator<E> implements Iterator<E> {
 	private int startIndex;
 	private int currentIndex;
 
+	public CircularIterator(E[] list) {
+		this(Arrays.asList(list));
+	}
+
+	public CircularIterator(List<E> list) {
+		this(list, 0);
+	}
+
+	public CircularIterator(List<E> list, E start) {
+		this(list, list.indexOf(start));
+	}
+
+	public CircularIterator(E[] list, E start) {
+		this(Arrays.asList(list), start);
+	}
+
 	public CircularIterator(List<E> list, int startIndex) {
 		super();
+
+		if (startIndex < 0 || startIndex >= list.size()) {
+			throw new ArrayIndexOutOfBoundsException("Start index is out of bounds: " + startIndex);
+		}
 
 		this.list = list;
 		this.startIndex = this.currentIndex = startIndex;
 	}
 
-	public CircularIterator(List<E> list, E start) {
-		super();
-
-		this.list = list;
-		this.startIndex = this.currentIndex = this.list.indexOf(start);
-	}
-
-	public CircularIterator(E[] list, E start) {
-		super();
-		
-		this.list = Arrays.asList(list);
-		this.startIndex = this.currentIndex = this.list.indexOf(start);
-	}
-	
 	public void reset() {
 		currentIndex = startIndex;
 	}
