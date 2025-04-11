dfd14531b98ad49f33f4a5e25a6512b7bb04a6f5
Sean Sundberg <seansund@us.ibm.com>
2025-04-11 10:41:25 -0500

    Set java version to 21
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/main/java/com/dex/midi/view/CompoundDecorator.java
+++ src/main/java/com/dex/midi/view/CompoundDecorator.java
@@ -1,27 +1,36 @@
 package com.dex.midi.view;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CompoundDecorator<T> implements Decorator<T> {
 	
-	private Decorator<T> defaultDecorator = null;
-	private List<? extends Decorator<T>> children = null;
+	private Decorator<T> defaultDecorator;
+	private List<? extends Decorator<T>> children;
 	
 	public CompoundDecorator(Decorator<T> defaultDecorator, Decorator<T>... dArray) {
 		this(defaultDecorator, Arrays.asList(dArray));
 	}
 	
 	public CompoundDecorator(Decorator<T> defaultDecorator, List<? extends Decorator<T>> children) {
-		this.defaultDecorator = defaultDecorator;
-		this.children = children;
+		this.defaultDecorator = defaultDecorator != null ? defaultDecorator : new NoopDecorator<T>();
+		this.children = children != null
+				? children.stream().filter(d -> Objects.nonNull(d)).collect(Collectors.toList())
+				: Arrays.asList();
 	}
 
 	@Override
 	public void decorate(T c) {
-		this.defaultDecorator.decorate(c);
+		defaultDecorator.decorate(c);
 
 		children.forEach((Decorator<T> d) -> d.decorate(c));
 	}
-	
+}
+
+class NoopDecorator<T> implements Decorator<T> {
+	public void decorate(T d) {
+		return;
+	}
 }
